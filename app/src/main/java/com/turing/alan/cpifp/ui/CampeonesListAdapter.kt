0dad09cdf5748e42d3d06402ce2502cd02da28d1
Veroonicagp <vgonpon3007@g.educaand.es>
2024-10-07 09:19:00 +0200

    .

--- app/src/main/java/com/turing/alan/cpifp/ui/CampeonesListAdapter.kt
+++ app/src/main/java/com/turing/alan/cpifp/ui/CampeonesListAdapter.kt
@@ -5,19 +5,24 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.turing.alan.cpifp.data.Champion
 import com.turing.alan.cpifp.databinding.CampeonListItemBinding
 
 class CampeonesListAdapter(): ListAdapter<Champion, CampeonesListAdapter.ChampionViewHolder>(ChampionDiffCallback) {
+    //ViewHolder: puente entre el xml y la informacion que tenemos
+    //inner: todos sus propiedades son visibles para la padre
     class ChampionViewHolder(private val binding: CampeonListItemBinding): RecyclerView.ViewHolder(binding.root){
         fun bind(champion: Champion){
             binding.winName.text=champion.name
             binding.title.text= champion.title
             binding.loren.text=champion.lore
+            binding.img.load(champion.imageUrl)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChampionViewHolder {
+        //intanciamos el xml y lo pasamos al itemViewHolder
         val binding: CampeonListItemBinding = CampeonListItemBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
@@ -32,8 +37,11 @@ class CampeonesListAdapter(): ListAdapter<Champion, CampeonesListAdapter.Champio
     object ChampionDiffCallback: DiffUtil.ItemCallback<Champion>(){
         override fun areItemsTheSame(oldItem: Champion,newItem: Champion) = oldItem.id == newItem.id
 
-        override fun areContentsTheSame(oldItem: Champion,newItem: Champion) = oldItem.name == newItem.name &&
+        override fun areContentsTheSame(oldItem: Champion,newItem: Champion) =
+                oldItem.name == newItem.name &&
                 oldItem.title == newItem.title &&
-                oldItem.lore == newItem.lore
+                oldItem.lore == newItem.lore &&
+                oldItem.imageUrl == newItem.imageUrl
     }
 }
+
